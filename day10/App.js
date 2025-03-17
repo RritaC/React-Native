20c41d2bbf249a681b76349ba7be27979056b8a4
RritaC <rritacanolli@gmail.com>
2025-03-17 20:51:57 +0100

    day10

---
+++ day10/App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Profile from './screens/Profile';
+
+export default function App() {
+  return (
+    <Profile />
+  );
+}

b55ddeaa8a3bb75980a995189f991d400f89b6e5
RritaC <rritacanolli@gmail.com>
2025-03-24 19:39:17 +0100

    first day12

---
+++ day11/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import StudentsProfile from './components/StudentsProfile';
+
+const App = () => {
+  return (
+    
+      <StudentsProfile />
+  );
+};
+
+export default App;
+
---
+++ day11/components/StudentsProfile.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native'
+
+const StudentsProfile = () => {
+    return (
+        
+    <ScrollView style={styles.container}>
+        <View style={styles.profileContainer}>
+            <Image style={styles.avatar} source={{ uri: "https://img.freepik.com/free-photo/woman-with-long-brown-hair_1308-179038.jpg" }} />
+            <View style={styles.profileDetail}>
+                <Text style={styles.name}>JANE DOE</Text>
+                <Text style={styles.name}>Email</Text>
+                <Text style={styles.role}>salmanbutt@gmail.com</Text>
+                <Text style={styles.name}>Graduation Date</Text>
+                <Text style={styles.role}>Dec 2018</Text>
+                <Text style={styles.name}>Degrees</Text>
+                <Text style={styles.role}>Bachelors /nr/
+                                            Masters /nr/
+                                            Post Graduate /nr/
+                                            PHD</Text>
+                <TouchableOpacity>
+                    <Text style={styles.hireText}>Hire ME!</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.projectContainer} >
+            <Image style={styles.projectImage} source={{ uri: "https://d3ui957tjb5bqd.cloudfront.net/uploads/2021/12/23072553/9-Project-Ideas-for-a-Graphic-Design-Portfolio_FeaturedImage_Horizontal-2.jpg" }} />
+            <Image style={styles.projectImage} source={{ uri: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2022/04/106032482.jpeg?auto=format&q=60&fit=max&w=930" }} />
+        </ScrollView>
+    </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fffff"
+    },
+    profileContainer: {
+        alignItems: "center",
+        padding: 20,
+        backgroundColor: "#f8f9fa",
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20
+    },
+    avatar: {
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+        marginBottom: 10
+    },
+    profileDetail: {
+        alignItems: "center",
+        fontWeight: "bold"
+    },
+    name: {
+        fontSize: 20,
+        fontWeight: bold
+    },
+    role: {
+        fontSize: 16,
+        color: "grey",
+        marginVertical: 5
+    },
+    description: {
+        textAlign: "center",
+        fontSize: 14,
+        marginBottom: 15
+    },
+    hireButton: {
+        backgroundColor: "FFD700",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 25
+    },
+    hireText: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "#000"
+    },
+    projectHeader: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        marginTop: 20
+    },
+    projectTitle: {
+        fontSize: 18,
+        fontWeight: "bold"
+    },
+    viewAll: {
+        fontSize: 14,
+        color: "#FFD700",
+        fontWeight: "bold"
+    },
+    projectContainer: {
+        flexDirection: "row",
+        paddingHorizontal: 20,
+        marginTop: 10
+    },
+    projectImage: {
+        width: 150,
+        height: 100,
+        borderRadius: 10,
+        marginRight: 10
+    }
+
+})
+
+
+export default StudentsProfile;
